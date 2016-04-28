65c4637c49661c060e969c54ac99c1ac69408f3c
Jan-Lucas Ott <ottj3@tcnj.edu>
2016-04-28 13:44:33 -0400

    Allow server url to be changed at runtime.
    Specifying "-Dsyncnote.server=127.0.0.1:80", for example, will cause
    the core code to connect to a local server to find user and note information
    (as dictated by the php files in the server directory). It will still default to
    the TCNJ webspace files.

--- synccore/src/main/java/insync/syncnote/Constants.java
+++ synccore/src/main/java/insync/syncnote/Constants.java
@@ -1,7 +1,15 @@
 package insync.syncnote;
 
-public class Constants {
+public final class Constants {
 
-    //public static final String SERVER = "http://tcnj.edu/~ottj3/";
-    public static final String SERVER = "http://127.0.0.1:80/";
+    static {
+        String serverName = System.getProperty("syncnote.server");
+        if (serverName != null) {
+            SERVER = serverName;
+        } else {
+            SERVER = "http://tcnj.edu/~ottj3/";
+        }
+    }
+
+    public static final String SERVER;
 }
