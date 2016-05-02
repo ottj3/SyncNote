1adbb3da02bc769ad624c869563788026d7521e9
Randell Carrido <carridr1@csmac24.lions.tcnj.edu>
2016-05-01 21:30:02 -0400

    adding final iOS folder

--- iOS/SyncNote/LoggedInViewController.swift
+++ iOS/SyncNote/LoggedInViewController.swift
@@ -1,4 +1,3 @@
-
 //
 //  ViewController.swift
 //  test
@@ -9,65 +8,294 @@
 
 import UIKit
 
+
 class LoggedInViewController: UIViewController {
-    
+    //This is the text that changes when you download your note
     @IBOutlet weak var text: UITextView!
     
+    
+    //This is the upload button that evenutally calls the upload functions to upload information to your notes stored online
     @IBAction func upload(sender: AnyObject) {
-        let syncText = text.text;
+        var new = text.text
+        //the url sent
+        let myUrl = NSURL(string: "http://www.tcnj.edu/~ottj3/login.php?username=default&password=password");let request = NSMutableURLRequest(URL:myUrl!);
+        request.HTTPMethod = "POST";
+        // Compose a query string
+        let postString = "";
+        request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding);
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
+            data, response, error in
+            //show request if there is no error
+            if error != nil
+            {
+                println("error=\(error)")
+                return
+            }
+            //Print out response object
+            println("response = \(response)")
+            // Print out response body
+            let responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
+            println("responseString = \(responseString)")
+            self.upload(responseString!, text: new)
+            println("YO")
+            //Convert response sent from a server side script to a NSDictionary object:
+            var err: NSError?
+            var myJSON = NSJSONSerialization.JSONObjectWithData(data, options: .MutableLeaves, error:&err) as? NSDictionary
+            
+            if let parseJSON = myJSON {
+                //Access value of First Name by its key
+                var firstNameValue = parseJSON["firstName"] as? String
+                println("firstNameValue: \(firstNameValue)")
+            }
+            
+        }
         
-    }
-
-    @IBAction func download(sender: AnyObject) {
-        text.text = ""
-        let syncText = text.text;
+        task.resume()
         
-    }
-    func dismissKeyboard() {
-        //Causes the view (or one of its embedded text fields) to resign the first responder status.
-        view.endEditing(true)
+        
     }
     
-    @IBAction func done(sender: UIButton) {
-    dismissKeyboard()
-    }
-
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    //////////////////////////////////////////////////////////////
+    //This is the upload function that is called by upload button to upload information to your notes stored online
+    
+    func upload(theKey: NSString, text: String) {
+        //send the url
+        if let myUrl = NSURL(string: "http://www.tcnj.edu/~ottj3/note.php?key=\(theKey)&message=\(text)") {
+            let request = NSMutableURLRequest(URL:myUrl);
+            request.HTTPMethod = "POST";
+            //OR GET
+            //request.HTTPMethod = "GET";
+            // Compose a query string
+            let postString = "";
+            request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding);
+            println("Here")
+            let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
+                data, response, error in
+                //show request if there is no error
+                if error != nil
+                {
+                    println("error=\(error)")
+                    return
+                }
+                println("Hello!")
+                // You can print out response object
+                println("response = \(response)")
+                // Print out response body
+                let responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
+                println("responseString = \(responseString)")
+                self.label.text = (responseString! as String)
+                //Convert response sent from a server side script to a NSDictionary object:
+                var err: NSError?
+                var myJSON = NSJSONSerialization.JSONObjectWithData(data, options: .MutableLeaves, error:&err) as? NSDictionary
+                if let parseJSON = myJSON {
+                    //Access value of First Name by its key
+                    var firstNameValue = parseJSON["firstName"] as? String
+                    println("firstNameValue: \(firstNameValue)")
+                }
+                println("Here")
+                
+            }
+            task.resume()
+            
+        }
         
-        let myUrl = NSURL(string: "http://www.tcnj.edu/~ottj3/notes.php");
-        let request = NSMutableURLRequest(URL:myUrl!);
+        
+    }
+    //////////////////////////////////////////////////////////////
+    
+    //This is the download button that evenutally calls the download functions to download information from your notes stored online
+    @IBAction func download(sender: UIButton) {
+        //label.text = text.text
+        var new = text.text
+        //send the url
+        let myUrl = NSURL(string: "http://www.tcnj.edu/~ottj3/login.php?username=default&password=password");let request = NSMutableURLRequest(URL:myUrl!);
         request.HTTPMethod = "POST";
         
         // Compose a query string
-        let postString = "?username=default&password=password";
+        let postString = "";
         
         request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding);
         
         let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
             data, response, error in
-            
-            if error != nil{
-                println(request)
+            //show request if there is no error
+            if error != nil
+            {
+                println("error=\(error)")
                 return
             }
             
             // You can print out response object
             println("response = \(response)")
-            
             // Print out response body
-            let responseString = NSString(data: data, encoding: NSUTF8StringEncoding)
+            let responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
             println("responseString = \(responseString)")
+            self.download(responseString!, text: new)
+            println("YO")
+            //Convert response sent from a server side script to a NSDictionary object:
+            var err: NSError?
+            var myJSON = NSJSONSerialization.JSONObjectWithData(data, options: .MutableLeaves, error:&err) as? NSDictionary
             
-            //Let’s convert response sent from a server side script to a NSDictionary object:
-        
+            if let parseJSON = myJSON {
+                // Access value of First Name by its key
+                var firstNameValue = parseJSON["firstName"] as? String
+                println("firstNameValue: \(firstNameValue)")
+            }
             
         }
         
         task.resume()
+        
+    }
+    
+    //////////////////////////////////////////////////////////////
+    
+    //This is the download function that is called by download button to download information from your notes stored online
+    func download(theKey: NSString, text: String) {
+        //send the url
+        if let myUrl = NSURL(string: "http://www.tcnj.edu/~ottj3/default.note?key=\(theKey)"){
+            let request = NSMutableURLRequest(URL:myUrl);
+            request.HTTPMethod = "POST";
+            //OR GET
+            //request.HTTPMethod = "GET";
+            // Compose a query string
+            let postString = "";
+            request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding);
+            println("Here")
+            let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
+                data, response, error in
+                //show request if there is no error
+                if error != nil
+                {
+                    println("error=\(error)")
+                    return
+                }
+                println("Hello!")
+                // You can print out response object
+                println("response = \(response)")
+                // Print out response body
+                let responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
+                println("responseString = \(responseString)")
+                self.label.text = (responseString! as String)
+                //self.sayHello(responseString!)
+                //Convert response sent from a server side script to a NSDictionary object:
+                var err: NSError?
+                var myJSON = NSJSONSerialization.JSONObjectWithData(data, options: .MutableLeaves, error:&err) as? NSDictionary
+                if let parseJSON = myJSON {
+                    //Access value of First Name by its key
+                    var firstNameValue = parseJSON["firstName"] as? String
+                    println("firstNameValue: \(firstNameValue)")
+                }
+                
+            }
+            task.resume()
+            
+        }
+        
+        
+    }
+    //////////////////////////////////////////////////////////
+    func logout(theKey: NSString) {
+        //send the url
+        if let myUrl = NSURL(string: "http://www.tcnj.edu/~ottj3/logout.php?key=\(theKey)") {
+            
+            let request = NSMutableURLRequest(URL:myUrl);
+            request.HTTPMethod = "POST";
+            //OR GET
+            //request.HTTPMethod = "GET";
+            // Compose a query string
+            let postString = "";
+            request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding);
+            let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
+                data, response, error in
+                //show request if there is no error
+                if error != nil
+                {
+                    println("error=\(error)")
+                    return
+                }
+                // You can print out response object
+                println("response = \(response)")
+                // Print out response body
+                let responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
+                println("responseString = \(responseString)")
+                self.label.text = (responseString! as String)
+                //Convert response sent from a server side script to a NSDictionary object:
+                var err: NSError?
+                var myJSON = NSJSONSerialization.JSONObjectWithData(data, options: .MutableLeaves, error:&err) as? NSDictionary
+                if let parseJSON = myJSON {
+                    //Access value of First Name by its key
+                    var firstNameValue = parseJSON["firstName"] as? String
+                    println("firstNameValue: \(firstNameValue)")
+                }
+                
+            }
+            task.resume()
+            
+        }
+        
+        
+    }
 
+    //////////////////////////////////////////////////////////////
+    @IBOutlet var label: UILabel!
+    //////////////////////////////////////////////////////////////
+    
+    @IBAction func logOut(sender: AnyObject) {
+        var new = text.text
+        //send the url
+        let myUrl = NSURL(string: "http://www.tcnj.edu/~ottj3/login.php?username=default&password=password");let request = NSMutableURLRequest(URL:myUrl!);
+        request.HTTPMethod = "POST";
+        
+        // Compose a query string
+        let postString = "";
+        
+        request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding);
+        
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
+            data, response, error in
+            //show request if there is no error
+            if error != nil
+            {
+                println("error=\(error)")
+                return
+            }
+            
+            // You can print out response object
+            println("response = \(response)")
+            // Print out response body
+            let responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
+            println("responseString = \(responseString)")
+            self.logout(responseString!)
+            println("YO")
+            //Convert response sent from a server side script to a NSDictionary object:
+            var err: NSError?
+            var myJSON = NSJSONSerialization.JSONObjectWithData(data, options: .MutableLeaves, error:&err) as? NSDictionary
+            
+            if let parseJSON = myJSON {
+                //Access value of First Name by its key
+                var firstNameValue = parseJSON["firstName"] as? String
+                println("firstNameValue: \(firstNameValue)")
+            }
+            
+        }
+        
+        task.resume()
+    }
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+    //the done button that has the keyboard dissappear
+    @IBAction func done(sender: UIButton) {
+        dismissKeyboard()
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
         
     }
     
@@ -78,5 +306,3 @@ class LoggedInViewController: UIViewController {
     
     
 }
-
-
--- iOS/SyncNote/ViewController.swift
+++ iOS/SyncNote/ViewController.swift
@@ -9,10 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //When you click done then the keyboard goes away
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
         view.addGestureRecognizer(tap)
     }
@@ -24,19 +24,63 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    //Below are all the buttons and text fields for the login page
+    //The blue backround image
     @IBOutlet weak var BackgroundBlue: UIImageView!
+    //The username text field
     @IBOutlet weak var username: UITextField!
+    //The password text field
     @IBOutlet weak var password: UITextField!
+    //The login button
     @IBAction func LoginRegister(sender: AnyObject) {
+        //set variables equal to the text entered by the user
         let user = username.text;
         let pass = password.text;
+        //Display an error message if the user does not enter a username or password
         if(user.isEmpty || pass.isEmpty){
+            //Alerts when missing username or password
             displayMyAlertMessage("Please type in both your username and password");
             return;
         }
+////////////////////////////////////////////////////////////////////////////
+        //if(user != nil || pass != nil){
+        //the url sent is the following url plus the entered information from the user
+        let myUrl = NSURL(string: "http://www.tcnj.edu/~ottj3/login.php?username="+user+"&password="+pass);
+        let request = NSMutableURLRequest(URL:myUrl!);
+        //Do a post method to get a key for login
+        request.HTTPMethod = "POST";
+        
+        // Compose a query string
+        let postString = "?username="+user+"&password="+pass;
+        request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding);
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
+            data, response, error in
+            //show request if there is no error
+            if error != nil{
+                println(request)
+                return
+            }
+            
+            // You can print out response object
+            println("response = \(response)")
+            
+            // Print out response body
+            let responseString = NSString(data: data, encoding: NSUTF8StringEncoding)
+            println("responseString = \(responseString)")
+            let rs = responseString!
+            println(rs);
+            if(rs as String == ""){
+                //self.displayMyAlertMessage("Please type in both your username and password");
+                self.dismissViewControllerAnimated(false, completion: nil);
+            }
 
+        }
+            task.resume()
 
+       
     }
+    
+//The function that display the error message if the user does not input any information
     func displayMyAlertMessage(userMessage:String)
     {
         
