82c2939fdbde1da2ee610385e68b09597e1d6e35
Jan-Lucas Ott <ottj3@tcnj.edu>
2016-05-02 10:48:48 -0400

    Add file headers.

--- synccore/src/main/java/insync/syncnote/exceptions/RequestInvalidException.java
+++ synccore/src/main/java/insync/syncnote/exceptions/RequestInvalidException.java
@@ -1,3 +1,9 @@
+/*
+ * SyncNote 2016
+ * CSC470 Final Project
+ * Jan-Lucas Ott, Connor Davis, Nate Harris, Randell Carrido
+ */
+
 package insync.syncnote.exceptions;
 
 public class RequestInvalidException extends Throwable {
